bfd956099fb5e2ad899fd260c740618b1eaf719c
Nakib Noor <tahfimism@gmail.com>
2025-07-08 13:52:21 +0600

    todo styling

--- pattho/activities/urls.py
+++ pattho/activities/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('todo/', views.todo_list, name='todo_list'),
     path('todo/add/', views.add_todo, name='add_todo'),
     path('todo/toggle/<int:todo_id>/', views.toggle_todo, name='toggle_todo'),
+    path('api/task-summary/', views.get_task_summary, name='api_task_summary'),
 ]
--- pattho/activities/views.py
+++ pattho/activities/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ToDoItem
 from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.utils import timezone
 
 @login_required
 def todo_list(request):
@@ -23,3 +25,23 @@ def toggle_todo(request, todo_id):
     todo.completed = not todo.completed
     todo.save()
     return redirect('todo_list')
+
+@login_required
+def get_task_summary(request):
+    user_todos = ToDoItem.objects.filter(user=request.user)
+    
+    total_tasks = user_todos.count()
+    completed_tasks = user_todos.filter(completed=True).count()
+    
+    today = timezone.localdate()
+    due_today_tasks = user_todos.filter(completed=False, date__date=today).count()
+    
+    overdue_tasks = user_todos.filter(completed=False, date__date__lt=today).count()
+    
+    data = {
+        'total_tasks': total_tasks,
+        'completed_tasks': completed_tasks,
+        'due_today_tasks': due_today_tasks,
+        'overdue_tasks': overdue_tasks,
+    }
+    return JsonResponse(data)
